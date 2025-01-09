7e0611c2ec531a50ff7b449891b9df92b014c901
JasonTer2003 <jasonterzhenhuang@gmail.com>
2025-01-09 20:04:47 +0800

    Update similar_songs.py

--- pages/similar_songs.py
+++ pages/similar_songs.py
@@ -6,7 +6,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 # Cache data loading
 @st.cache_data
 def load_data():
-    df = pd.read_csv(r'C:\Drive D\UM\Y3S1\WIH3001 DSP\Latest Project\df_for_recommender.csv')
+    df = pd.read_csv('df_for_recommender.csv')
     return df
 
 data = load_data()
