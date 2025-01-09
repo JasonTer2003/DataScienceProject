407973d740c9a4591c0c5951a12665cbec82c4ad
JasonTer2003 <jasonterzhenhuang@gmail.com>
2025-01-09 19:52:31 +0800

    Update visualization.py

--- pages/visualization.py
+++ pages/visualization.py
@@ -7,20 +7,20 @@ from PIL import Image
 # Cache data loading
 @st.cache_data
 def load_data():
-    df = pd.read_csv(r'C:\Drive D\UM\Y3S1\WIH3001 DSP\Latest Project\df_for_recommender.csv')
+    df = pd.read_csv('df_for_recommender.csv')
     return df
 
 data = load_data()
 
 def listening_habits_dashboard():
     # Display static images
-    img = Image.open(r"C:\Drive D\UM\Y3S1\WIH3001 DSP\Latest Project\listening_habits_dashboard.png")
+    img = Image.open('listening_habits_dashboard.png")
     st.image(img, caption="Listening Habits of Malaysia Listeners")
 
 
 def worldwide_dashboard():
     # Display static images
-    img = Image.open(r"C:\Drive D\UM\Y3S1\WIH3001 DSP\Latest Project\worldwide_dashboard.png")
+    img = Image.open('worldwide_dashboard.png")
     st.image(img, caption="Analysis of Song Features Worldwide Dashboard")
 
 st.title("\U0001F4CA Visualization")
